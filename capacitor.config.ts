30aa718beb5917a7fd9083b75f419bdf59c0138b
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2025-06-04 17:34:47 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'insulin',
+  webDir: 'dist'
+};
+
+export default config;
